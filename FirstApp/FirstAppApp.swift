7fdc5d34dd678b918466e9abb12f3f1ea541d6d8
AlexVcom <doalvor@gmial.com>
2024-03-17 21:28:39 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Koty on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
