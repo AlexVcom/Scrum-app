a7edca838395d4f942508036f2515afb9d2aea36
AlexVcom <doalvor@gmial.com>
2024-03-30 20:49:34 +0100

    Add error handler

---
+++ ScrumDingerApp/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  ScrumDingerApp
+//
+//  Created by Koty on 28/03/2024.
+//
+
+import Foundation
---
+++ ScrumDingerApp/Views/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  ScrumDingerApp
+//
+//  Created by Koty on 28/03/2024.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
